ea6873aadf0824eeb744eb353fb768a6ee8ff796
Peter-Jan Celis <pj@celis.org>
2013-04-09 02:20:34 +0200

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Judgejam::Application.config.secret_token = '44368770d409c3cf86998db63e17a1e06ce4428db24c838fad53ed62cc9430c95b5213259cb1858319f4a6b475bc0bf5d7ae14d625d5959653746559310951d1'
